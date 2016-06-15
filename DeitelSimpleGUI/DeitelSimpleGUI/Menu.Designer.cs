0c9c4192dd5bd259d0759a21c581513667431665
frizob <rizo23@hotmail.com>
2016-06-15 10:00:37 -0600

    Add files via upload

---
+++ DeitelSimpleGUI/DeitelSimpleGUI/Menu.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DeitelSimpleGUI
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botAceptar
+            // 
+            this.botAceptar.Location = new System.Drawing.Point(80, 119);
+            this.botAceptar.Name = "botAceptar";
+            this.botAceptar.Size = new System.Drawing.Size(125, 23);
+            this.botAceptar.TabIndex = 0;
+            this.botAceptar.Text = "Presione Aqui";
+            this.botAceptar.UseVisualStyleBackColor = true;
+            this.botAceptar.Click += new System.EventHandler(this.botAceptar_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.botAceptar);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botAceptar;
+    }
+}
+
---
+++ DeitelSimpleGUI/DeitelSimpleGUI/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeitelSimpleGUI
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void botAceptar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esta ventana aparece cuando se presiona el boton");
+        }
+    }
+}
